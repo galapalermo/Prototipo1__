76447d2c53e1e4b6d0d20cd85af4f0454cb1ff9a
galapalermo <galapalermo02@gmail.com>
2026-05-21 12:16:30 -0300

    terminamos

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
 {
+    public int score= 0;
+    public UIManager uiManagerScript;
+    void Awake()
+    {
+        uiManagerScript = GameObject.FindObjectOfType<UIManager>();
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Collectible")){
             Destroy(other.gameObject);
+            score ++; 
+            uiManagerScript.UpdateScore(score);
         }
-        Debug.Log(other.gameObject.name);
+            
+        
     }
 
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+
+    void Start()
+    {
+        //poner el score en cero
+        UpdateScore(0);
+    
+    }
+
+    public void UpdateScore(int score)
+    {
+        //actualizar el texto score
+        txtScore.text = score.ToString();
+    }
+}
